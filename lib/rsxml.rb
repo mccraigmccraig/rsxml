f7a7ed6ea631eae2da8cf3421b04c510a0b8958c
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2011-05-04 12:37:14 +0100

    rsxml : convert XML docs to s-expressions and back again

---
+++ lib/rsxml.rb
@@ -0,0 +1,102 @@
+require 'nokogiri'
+require 'builder'
+
+module Rsxml
+  module_function
+
+  # convert an s-expression representation of an XML document to XML
+  #  Rsxml.to_xml(["Foo", {"foofoo"=>"10"}, ["Bar", "barbar"]]) =>
+  #    "<Foo foofoo=\"10\"><Bar>barbar</Bar></Foo>"
+  def to_xml(rsxml)
+    xml = Builder::XmlMarkup.new
+    Sexp.write_xml(xml, rsxml)
+    xml.target!
+  end
+
+  # convert an XML string to an s-expression representation
+  #  Rsxml.to_rsxml("<Foo foofoo=\"10\"><Bar>barbar</Bar></Foo>") =>
+  #    ["Foo", {"foofoo"=>"10"}, ["Bar", "barbar"]]
+  def to_rsxml(doc)
+    root = Nokogiri::XML(doc).children.first
+    Xml.read_xml(root, [])
+  end
+
+  module Sexp
+    module_function
+
+    def write_xml(xml, sexp)
+      tag, attrs, children = decompose_sexp(sexp)
+      
+      xml.__send__(tag, attrs) do
+        children.each do |child|
+          if child.is_a?(Array)
+            write_xml(xml, child)
+          else
+            xml << child
+          end
+        end
+      end
+    end
+
+    def decompose_sexp(sexp)
+      raise "invalid rsxml: #{rsxml.inspect}" if sexp.length<1
+      tag = sexp[0].to_s
+      if sexp[1].is_a?(Hash)
+        attrs = sexp[1]
+        children = sexp[2..-1]
+      else
+        attrs = {}
+        children = sexp[1..-1]
+      end
+      [tag, attrs, children]
+    end
+  end
+
+  module Xml
+    module_function
+
+    def read_xml(node, ns_stack)
+      prefix = node.namespace.prefix if node.namespace
+      tag = node.name
+      ns_tag = [prefix,tag].compact.join(":")
+
+      attrs = read_attributes(node.attributes)
+      attrs = attrs.merge(namespace_attributes(node.namespaces, ns_stack))
+      attrs = nil if attrs.empty?
+
+      children = node.children.map do |child|
+        if child.text?
+          child.text
+        else
+          begin
+            ns_stack.push(node.namespaces)
+            read_xml(child, ns_stack)
+          ensure
+            ns_stack.pop
+          end
+        end
+      end
+
+      [ns_tag, attrs, *children].compact
+    end
+
+    def read_attributes(attrs)
+      Hash[*attrs.map do |n, attr|
+             prefix = attr.namespace.prefix if attr.namespace
+             name = attr.name
+             ns_name = [prefix,name].compact.join(":")
+             [ns_name, attr.value]
+           end.flatten]
+    end
+
+    def namespace_attributes(namespaces, ns_stack)
+      Hash[*namespaces.map do |prefix,href|
+             [prefix, href] if !find_namespace(prefix, ns_stack)
+           end.compact.flatten]
+    end
+
+    def find_namespace(prefix, ns_stack)
+      ns_stack.reverse.find{ |nsh| nsh.has_key?(prefix)}
+    end
+  end
+end
--- spec/rsxml_spec.rb
+++ spec/rsxml_spec.rb
@@ -1,7 +1,103 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Rsxml" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Rsxml do
+  describe "to_xml" do
+    it "should produce a single-element document" do
+      Rsxml.to_xml([:foo]).should == "<foo></foo>"
+    end
+
+    it "should produce a single-element doc with attrs" do
+      xml = Rsxml.to_xml([:foo, {:bar=>1, :baz=>"baz"}])
+      r = Nokogiri::XML(xml).children.first
+      r.name.should == "foo"
+      r["bar"].should == "1"
+      r["baz"].should == "baz"
+    end
+
+    it "should produce a doc with text content" do
+      xml = Rsxml.to_xml([:foo, "foofoo"])
+      r = Nokogiri::XML(xml).children.first
+      r.name.should == "foo"
+      r.children.size.should == 1
+      txt = r.children.first
+      txt.text?.should == true
+      txt.text.should == "foofoo"
+    end
+
+    it "should produce a doc with child elements" do
+      xml = Rsxml.to_xml([:foo, [:bar], [:baz]])
+      r = Nokogiri::XML(xml).children.first
+      r.name.should == "foo"
+      r.children.length.should == 2
+      bar = r.children.first
+      bar.name.should == "bar"
+      bar.children.length.should == 0
+      baz = r.children[1]
+      baz.name.should == "baz"
+      baz.children.length.should == 0
+    end
+
+    it "should produce a doc with child elements and attributes" do
+      xml = Rsxml.to_xml([:foo, [:bar, {:barbar=>"boo", :bazbaz=>"baz"}]])
+      r = Nokogiri::XML(xml).children.first
+      r.name.should == "foo"
+      r.children.length.should == 1
+      bar = r.children.first
+      bar.name.should == "bar"
+      bar.children.length.should == 0
+      bar["barbar"].should == "boo"
+      bar["bazbaz"].should == "baz"
+    end
+
+    it "should treat namespace prefixes reasonably" do
+      xml = Rsxml.to_xml(["foo:foofoo", {"xmlns:foo"=>"http://foo.com/foo", "foo:bar"=>1, "foo:baz"=>"baz"}])
+
+      r = Nokogiri::XML(xml).children.first
+      r.namespaces["xmlns:foo"].should == "http://foo.com/foo"
+
+      r.name.should == "foofoo"
+      r.namespace.href.should == "http://foo.com/foo"
+      r.namespace.prefix.should == "foo"
+
+      r["bar"].should == "1"
+      r.attributes["bar"].namespace.href.should == "http://foo.com/foo"
+      r.attributes["bar"].namespace.prefix.should == "foo"
+
+      r["baz"].should == "baz"
+      r.attributes["baz"].namespace.href.should == "http://foo.com/foo"
+      r.attributes["baz"].namespace.prefix.should == "foo"
+    end
+  end
+
+  describe "to_rsxml" do
+    def test_roundtrip(org) 
+      xml = Rsxml.to_xml(org)
+      rsxml = Rsxml.to_rsxml(xml)
+      rsxml.should == org
+    end
+    
+    it "should parse a single-element doc" do
+      test_roundtrip(["foo"])
+    end
+
+    it "should parse a single-element doc with attributes" do
+      test_roundtrip(["foo", {"bar"=>"1", "baz"=>"bazbaz"}])
+    end
+
+    it "should parse a doc with child elements" do
+      test_roundtrip(["foo", ["bar"], ["baz", ["boo"]]])
+    end
+
+    it "should parse a doc with text content" do
+      test_roundtrip(["foo", "foofoo"])
+    end
+
+    it "should parse a doc with child elements and attributes" do
+      test_roundtrip(["foo", {"bar"=>"1", "baz"=>"bazbaz"}, ["foofoo", {"foobar"=>"3", "barbaz"=>"4"}, "foofoofoo"]])
+    end
+
+    it "should parse a doc with namespaces" do
+      test_roundtrip(["foo:foofoo", {"xmlns:foo"=>"http://foo.com/foo", "foo:bar"=>"1", "foo:baz"=>"baz"}])
+    end
   end
 end
