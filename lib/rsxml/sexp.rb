b3acec1d96f0cade4ba95e87f674300736fbb3ee
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2011-05-10 13:28:43 +0100

    visitor pattern on Sexp tree

--- lib/rsxml/sexp.rb
+++ lib/rsxml/sexp.rb
@@ -1,6 +1,100 @@
 module Rsxml
   module Sexp
+    class Context
+      attr_reader :ns_stack
+      attr_reader :node_stack
+      attr_reader :prev_siblings
+      def initialize()
+        @ns_stack=[]
+        @node_stack=[]
+        @prev_siblings=[]
+        @sibling_stack=[]
+      end
+
+      def push_node(node)
+        node_stack.push(node)
+        @sibling_stack.push(@prev_siblings)
+        @prev_siblings=[]
+      end
+
+      def pop_node
+        n = node_stack.pop
+        @prev_siblings = @sibling_stack.pop
+        @prev_siblings << n
+      end
+
+      def processed_node(node)
+        @prev_siblings << node
+      end
+    end
+
+    class WriteXmlVisitor
+      attr_reader :xml
+      def initialize()
+        @xml = Builder::XmlMarkup.new
+      end
+
+      def tag(context, qname, qattrs)
+        xml.__send__(qname, qattrs) do
+          yield
+        end
+      end
+
+      def text(context, text)
+        xml << text
+      end
+
+      def to_s
+        xml.target!
+      end
+    end
+
     module_function
+
+    def traverse(sexp, visitor, context=Context.new)
+      tag, attrs, children = decompose_sexp(sexp)
+      
+      ns_declared = Namespace::extract_declared_namespace_bindings(attrs)
+
+      ns_stack_decl = context.ns_stack + [ns_declared]
+      etag = Namespace::explode_qname(ns_stack_decl, tag)
+      eattrs = Namespace::explode_attr_qnames(ns_stack_decl, attrs)
+
+      # figure out which explicit namespaces need declaring
+      ns_explicit = Namespace::extract_explicit_namespace_bindings(etag, eattrs)
+      ns_undeclared = Namespace::undeclared_namespace_bindings(ns_stack_decl, ns_explicit)
+      ns_undeclared_decls = Namespace::exploded_namespace_declarations(ns_undeclared)
+
+      eattrs = eattrs.merge(ns_undeclared_decls)
+      ns_new_bindings = Namespace::merge_namespace_bindings(ns_declared, ns_undeclared)
+
+      context.ns_stack.push(ns_new_bindings)
+      begin
+        qname = Namespace::compact_qname(context.ns_stack, etag)
+        qattrs = Namespace::compact_attr_qnames(context.ns_stack, eattrs)
+
+        visitor.tag(context, qname, qattrs) do
+          begin
+          context.push_node([qname, qattrs])
+            children.each_with_index do |child, i|
+              if child.is_a?(Array)
+                traverse(child, visitor, context)
+              else
+                visitor.text(context, child)
+                context.processed_node(child)
+              end
+            end
+          ensure
+            context.pop_node
+          end
+        end
+
+      ensure
+        context.ns_stack.pop
+      end
+
+      visitor
+    end
 
     def write_xml(xml, sexp, ns_stack=[], path=[""], &transformer)
       tag, attrs, children = decompose_sexp(sexp)
--- spec/rsxml/sexp_spec.rb
+++ spec/rsxml/sexp_spec.rb
@@ -1,4 +1,22 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 module Rsxml
+  describe Sexp::WriteXmlVisitor do
+    it "should write single element xml" do
+      Sexp::traverse(["item"], Sexp::WriteXmlVisitor.new).to_s.should == "<item></item>"
+    end
+
+    it "should write single element xml with attributes" do
+      Sexp::traverse([:item, {:foo=>"100"}], Sexp::WriteXmlVisitor.new).to_s.should == '<item foo="100"></item>'
+    end
+
+    it "should write nested elements with attributes and text content" do
+      Sexp::traverse([:item, {:foo=>"100"}, [:bar], "foofoo", [:baz]], Sexp::WriteXmlVisitor.new).to_s.should == '<item foo="100"><bar></bar>foofoo<baz></baz></item>'
+    end
+  end
+
+
+  describe "traverse" do
+    
+  end
 end
