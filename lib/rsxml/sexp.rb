c255d2d61ba2cf409573f3baf2f8cd6f9884ead6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2011-05-10 11:22:11 +0100

    splitting namespace related functions into Rsxml::Namespace module

--- lib/rsxml/sexp.rb
+++ lib/rsxml/sexp.rb
@@ -1,5 +1,9 @@
 module Rsxml
   module Sexp
+    class << self
+      include Namespace
+    end
+
     module_function
 
     def write_xml(xml, sexp, ns_stack=[], path=[""], &transformer)
@@ -50,157 +54,6 @@ module Rsxml
       ensure
         ns_stack.pop
       end
-    end
-
-    # namespace qualify attrs
-    def compact_attr_qnames(ns_stack, attrs)
-      Hash[attrs.map do |name,value|
-             [compact_qname(ns_stack, name), value]
-           end]
-    end
-
-    def explode_attr_qnames(ns_stack, attrs)
-      Hash[attrs.map do |name, value|
-             uq_name = explode_qname(ns_stack, name, true)
-             local_name, prefix, uri = uq_name
-             if !prefix || prefix==""
-               [local_name, value]
-             else
-               [uq_name, value]
-             end
-           end]
-    end
-
-    # produce a QName from a [LocalPart, prefix, URI] triple
-    def compact_qname(ns_stack, name)
-      return name if name.is_a?(String)
-
-      local_part, prefix, uri = name
-      raise "invalid name: #{name}" if !prefix && uri
-      if prefix
-        if prefix!="xmlns"
-          ns = find_namespace_uri(ns_stack, prefix, uri)
-          raise "namespace prefix not bound to a namespace: '#{prefix}'" if ! ns
-        end
-        [prefix, local_part].map{|s| s.to_s unless s.to_s.empty?}.compact.join(':')
-      else
-        local_part
-      end
-    end
-
-    # split a QName into [LocalPart, prefix and URI] triple
-    def explode_qname(ns_stack, qname, attr=false)
-      if qname.is_a?(Array)
-        if qname.length>1 && !qname[1].nil?
-          return qname
-        elsif qname.length>1 && qname[1].nil? && !qname[2].nil?
-          raise "invalid name: #{qname.inspect}"
-        else
-          return qname[0]
-        end
-      end
-
-      local_part, prefix = split_qname(qname)
-      if prefix
-        if prefix=="xmlns" && attr
-          [local_part, prefix]
-        else
-          uri = find_namespace_uri(ns_stack, prefix)
-          raise "namespace prefix not bound: '#{prefix}'" if ! uri
-          [local_part, prefix, uri]
-        end
-      else
-        if attr
-          local_part
-        else
-          default_uri = find_namespace_uri(ns_stack, "")
-          if default_uri
-            [local_part, "", default_uri]
-          else
-            local_part
-          end
-        end
-      end
-    end
-
-    # split a qname String into a [local_part, prefix] pair
-    def split_qname(qname)
-      return qname if qname.is_a?(Array)
-
-      if qname =~ /^[^:]+:[^:]+$/
-        [*qname.split(':')].reverse
-      else
-        qname
-      end
-    end
-
-    # returns the namespace uri for a prefix, if declared in the stack
-    def find_namespace_uri(ns_stack, prefix, uri_check=nil)
-      tns = ns_stack.reverse.find{|ns| ns.has_key?(prefix)}
-      uri = tns[prefix] if tns
-      raise "prefix: '#{prefix}' is bound to uri: '#{uri}', but should be '#{uri_check}'" if uri_check && uri && uri!=uri_check
-      uri
-    end
-
-
-    # extract a Hash of {prefix=>uri} mappings declared in attributes
-    def extract_declared_namespace_bindings(attrs)
-      Hash[attrs.map do |name,value|
-             local_part, prefix, uri = split_qname(name)
-             if (prefix && prefix == "xmlns")
-               [local_part, value]
-             elsif (!prefix && local_part == "xmlns")
-               ["", value]
-             end
-           end.compact]
-    end
-
-    # extract a Hash of {prefix=>uri} mappings from expanded tags
-    def extract_explicit_namespace_bindings(tag, attrs)
-      tag_local_part, tag_prefix, tag_uri = tag
-      ns = {}
-      ns[tag_prefix] = tag_uri if tag_prefix && tag_uri
-
-      attrs.each do |name, value|
-        attr_local_part, attr_prefix, attr_uri = name
-        if attr_prefix && attr_uri
-          raise "bindings clash: '#{attr_prefix}'=>'#{ns[attr_prefix]}' , '#{attr_prefix}'=>'#{attr_uri}'" if ns.has_key?(attr_prefix) && ns[attr_prefix]!=attr_uri
-          ns[attr_prefix] = attr_uri
-        end
-      end
-      ns
-    end
-    
-    # figure out which explicit namespaces need declaring
-    #
-    # +ns_stack+ is the stack of namespace bindings
-    # +ns_explicit+ is the explicit refs for a tag
-    def undeclared_namespaces(ns_stack, ns_explicit)
-      Hash[ns_explicit.map do |prefix,uri|
-             [prefix, uri] if !find_namespace_uri(ns_stack, prefix, uri)
-           end.compact]
-    end
-
-    # produce a Hash of namespace declaration attributes from 
-    # a Hash of namespace prefix bindings
-    def unqualified_namespace_declarations(ns)
-      Hash[ns.map do |prefix, uri|
-             if prefix==""
-               ["xmlns", uri]
-             else
-               [[prefix, "xmlns"], uri]
-             end
-           end]
-    end
-
-    # merges two sets of namespace bindings, raising error on clash
-    def merge_namespace_bindings(ns1, ns2)
-      m = ns1.clone
-      ns2.each do |k,v|
-        raise "bindings clash: '#{k}'=>'#{m[k]}' , '#{k}'=>'#{v}'" if m.has_key?(k) && m[k]!=v
-        m[k]=v
-      end
-      m
     end
 
     def decompose_sexp(sexp)
--- spec/rsxml/namespace_spec.rb
+++ spec/rsxml/namespace_spec.rb
@@ -1,4 +1,228 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 module Rsxml
+    describe "find_namespace_uri" do
+      it "should find a namespace uri in a stack of prefix bindings" do
+        Rsxml::Namespace.find_namespace_uri([{"foo"=>"http://foo.com/foo"},
+                                    {"bar"=>"http://bar.com/bar"}],
+                                   "bar").should == "http://bar.com/bar"
+        Rsxml::Namespace.find_namespace_uri([{"foo"=>"http://foo.com/foo"},
+                                    {"bar"=>"http://bar.com/bar"}],
+                                   "foo").should == "http://foo.com/foo"
+      end
+
+      it "should return nil if there is no matching binding" do
+        Rsxml::Namespace.find_namespace_uri([{"foo"=>"http://foo.com/foo"},
+                                    {"bar"=>"http://bar.com/bar"}],
+                                   "blah").should == nil
+        Rsxml::Namespace.find_namespace_uri([],
+                                   "blah").should == nil
+      end
+
+    end
+
+    describe "explode_qname" do
+      it "should do nothing to an array with more than one element" do
+        Rsxml::Namespace.explode_qname([], ["bar", "foo", "http://foo.com/foo"]).should ==
+          ["bar", "foo", "http://foo.com/foo"]
+        Rsxml::Namespace.explode_qname([], ["bar", "foo"]).should ==
+          ["bar", "foo"]
+      end
+
+      it "should return the first element of an array with only the first non-nil element" do
+        Rsxml::Namespace.explode_qname([], ["bar"]).should ==
+          "bar"
+      end
+
+      it "should raise an error if prefix is nil but uri is not" do
+        lambda {
+          Rsxml::Namespace.explode_qname([], ["bar", nil, "http://foo.com/foo"])
+        }.should raise_error(/invalid name/)
+      end
+
+      it "should unqualify an unprefixed name with no default namespace" do
+        Rsxml::Namespace.explode_qname([], "bar").should == "bar"
+      end
+
+      it "should unqualify an unprefixed name in the default namespace" do
+        Rsxml::Namespace.explode_qname([{""=>"http://foo.com/foo"}], "bar").should == 
+          ["bar", "", "http://foo.com/foo"]
+      end
+
+      it "should unqualify a prefixed name with a bound namespace" do
+        Rsxml::Namespace.explode_qname([{"foo"=>"http://foo.com/foo"}], "foo:bar").should ==
+          ["bar", "foo", "http://foo.com/foo"]
+      end
+
+      it "should raise an error with a prefixed name with no bound namespace" do
+        lambda {
+          Rsxml::Namespace.explode_qname([], "foo:bar")
+        }.should raise_error(/not bound/)
+      end
+    end
+
+    describe "split_qname" do
+      it "should split a name with prefix" do
+        Rsxml::Namespace.split_qname("foo:bar").should == ["bar", "foo"]
+      end
+
+      it "should leave a name without prefix unchanged" do
+        Rsxml::Namespace.split_qname("bar").should == "bar"
+      end
+
+      it "should leave an array unchanged" do
+        Rsxml::Namespace.split_qname(["bar", "foo"]).should == ["bar", "foo"]
+      end
+    end
+
+    describe "compact_attr_qnames" do
+      it "should qualify attribute names when there is a default namespaces" do
+        Rsxml::Namespace.compact_attr_qnames([{""=>"http://default.com/default","foo"=>"http://foo.com/foo"}], {["bar", "foo"] => "barbar", ["boo", ""] => "booboo", "baz" =>"bazbaz"}).should ==
+          {"foo:bar"=>"barbar", "boo"=>"booboo", "baz"=>"bazbaz"}
+      end
+
+      it "should raise an exception if a namespace is referenced but not bound" do
+        lambda {
+          Rsxml::Namespace.compact_attr_qnames([], {["bar", "foo"] => "barbar", "baz" =>"bazbaz"})
+        }.should raise_error(/not bound/)
+      end
+
+      it "should raise an exception if default namespace referenced but not bound" do
+        lambda {
+          Rsxml::Namespace.compact_attr_qnames([{"foo"=>"http://foo.com/foo"}], {["bar", "foo"] => "barbar", ["boo", ""] => "booboo", "baz" =>"bazbaz"})
+        }.should raise_error(/not bound/)
+      end
+    end
+
+    describe "explode_attr_qnames" do
+      it "should unqualify attributes when there is no default namespace" do
+        Rsxml::Namespace.explode_attr_qnames([{"foo"=>"http://foo.com/foo"}], {"foo:bar"=>"barbar", "baz"=>"bazbaz"}).should ==
+          {["bar", "foo", "http://foo.com/foo"]=>"barbar", "baz"=>"bazbaz"}
+      end
+
+      it "should unqualify attributes when there is a default namespace" do
+        Rsxml::Namespace.explode_attr_qnames([{""=>"http://default.com/default", "foo"=>"http://foo.com/foo"}], {"foo:bar"=>"barbar", "baz"=>"bazbaz"}).should ==
+          {["bar", "foo", "http://foo.com/foo"]=>"barbar", "baz"=>"bazbaz"}
+      end
+    end
+
+    describe "compact_qname" do
+      it "should produce a qname from a pair or triple" do
+        Rsxml::Namespace.compact_qname([{"foo"=>"http://foo.com/foo"}], 
+                                 ["bar", "foo"]).should ==
+          "foo:bar"
+        Rsxml::Namespace.compact_qname([{"foo"=>"http://foo.com/foo"}], 
+                                 ["bar", "foo", "http://foo.com/foo"]).should ==
+          "foo:bar"
+      end
+
+      it "should produce a qname with default namespace" do
+        Rsxml::Namespace.compact_qname([{""=>"http://foo.com/foo"}], 
+                                 ["bar", ""]).should ==
+          "bar"
+
+        Rsxml::Namespace.compact_qname([{""=>"http://foo.com/foo"}], 
+                                 ["bar", "", "http://foo.com/foo"]).should ==
+          "bar"
+      end
+
+      it "should do nothing to a String" do
+        Rsxml::Namespace.compact_qname([], "foo:bar").should == "foo:bar"
+      end
+
+      it "should raise an error if prefix is nil but uri is not" do
+        lambda {
+          Rsxml::Namespace.compact_qname([], ["bar", nil, "http://foo.com/foo"])
+        }.should raise_error(/invalid name/)
+      end
+
+      it "should raise an error if a prefix is not bound" do
+        lambda {
+          Rsxml::Namespace.compact_qname([], 
+                                   ["bar", "foo", "http://foo.com/foo"])
+        }.should raise_error(/not bound/)
+      end
+
+      it "should raise an error if a prefix binding clashes" do
+        lambda {
+          Rsxml::Namespace.compact_qname([{"foo"=>"http://foo.com/foo"}], 
+                                   ["bar", "foo", "http://bar.com/bar"])
+        }.should raise_error(/'foo' is bound/)
+      end
+
+      it "should raise an error if default namespace binding clashes" do
+        lambda {
+          Rsxml::Namespace.compact_qname([{""=>"http://foo.com/foo"}], 
+                                   ["bar", "", "http://bar.com/bar"])
+        }.should raise_error(/'' is bound/)
+      end
+
+    end
+
+    describe "extract_declared_namespace_bindings" do
+      it "should extract a hash of declared namespace bindings from a Hash of attributes" do
+        Rsxml::Namespace.extract_declared_namespace_bindings({"xmlns"=>"http://default.com/default", "xmlns:foo"=>"http://foo.com/foo", "foo"=>"bar"}).should ==
+          {""=>"http://default.com/default", "foo"=>"http://foo.com/foo"}
+      end
+    end
+
+    describe "extract_explicit_namespace_bindings" do
+      it "should extract a hash of explicit namespace bindings from expanded tags" do
+        Rsxml::Namespace.extract_explicit_namespace_bindings(["bar", "foo", "http://foo.com/foo"],
+                                                {["baz", "bar", "http://bar.com/bar"]=>"baz",
+                                                  ["boo", "", "http://default.com/default"]=>"boo"}).should ==
+          {"foo"=>"http://foo.com/foo", "bar"=>"http://bar.com/bar", ""=>"http://default.com/default"}
+      end
+
+      it "should raise an error if extracted namespaces clash" do
+        lambda{
+          Rsxml::Namespace.extract_explicit_namespace_bindings(["bar", "foo", "http://foo.com/foo"],
+                                                  {["baz", "foo", "http://bar.com/bar"]=>"baz",
+                                                    ["boo", "", "http://default.com/default"]=>"boo"})
+        }.should raise_error(/bindings clash/)
+      end
+    end
+
+    describe "undeclared_namespaces" do
+      it "should determine which explicit namespaces are not declared in context" do
+        Rsxml::Namespace.undeclared_namespaces([{""=>"http://default.com/default", "foo"=>"http://foo.com/foo"}], {""=>"http://default.com/default", "foo"=>"http://foo.com/foo", "bar"=>"http://bar.com/bar"}).should ==
+          {"bar"=>"http://bar.com/bar"}
+      end
+
+      it "should raise an exception if an explicit prefix clashes with a declard prefix" do
+        lambda {
+          Rsxml::Namespace.undeclared_namespaces([{""=>"http://default.com/default", "foo"=>"http://foo.com/foo"}], {""=>"http://foo.com/foo", "foo"=>"http://foo.com/foo", "bar"=>"http://bar.com/bar"})
+        }.should raise_error(/is bound to uri/)
+      end
+    end
+
+    describe "merge_namespace_bindings" do
+      it "should merge two sets of namespace bindings" do
+        Rsxml::Namespace.merge_namespace_bindings({""=>"http://default.com/default",
+                                               "boo"=>"http://boo.com/boo"},
+                                             {"foo"=>"http://foo.com/foo",
+                                               "bar"=>"http://bar.com/bar"}).should ==
+          {""=>"http://default.com/default",
+          "boo"=>"http://boo.com/boo",
+          "foo"=>"http://foo.com/foo",
+          "bar"=>"http://bar.com/bar"}
+      end
+
+      it "should raise an error if there are binding clashes" do
+        lambda {
+          Rsxml::Namespace.merge_namespace_bindings({""=>"http://default.com/default",
+                                                 "boo"=>"http://boo.com/boo"},
+                                               {""=>"http://foo.com/foo",
+                                                 "bar"=>"http://bar.com/bar"})
+        }.should raise_error(/bindings clash/)
+      end
+    end
+
+    describe "unqualified_namespace_declarations" do
+      it "should produce unqalified namespaces declarations" do
+        Rsxml::Namespace.unqualified_namespace_declarations({""=>"http://default.com/default", "foo"=>"http://foo.com/foo"}).should == 
+          {"xmlns"=>"http://default.com/default", ["foo", "xmlns"]=>"http://foo.com/foo"}
+      end
+    end
+
 end
